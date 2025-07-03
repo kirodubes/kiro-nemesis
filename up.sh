22a469bb555ce9aa657340cd0c9498bdfdca381f
Erik Dubois <erik.dubois@gmail.com>
2025-07-03 06:49:51 +0200

    update

--- up.sh
+++ up.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-#set -e
+set -eo pipefail
+##################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Youtube   : https://youtube.com/erikdubois
+# Github    : https://github.com/erikdubois
+# Github    : https://github.com/kirodubes
+# Github    : https://github.com/buildra
+# SF        : https://sourceforge.net/projects/kiro/files/
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
 
+# variables and functions
 workdir=$(pwd)
-dir="calamares-3.3.14.r25.g95aa33f"
-source="/home/erik/KIRO/kiro-pkgbuild/"
-destiny="/home/erik/KIRO/kiro-calamares-config/etc/calamares/pkgbuild/"
-
-rm -r /home/erik/KIRO/kiro-calamares-config/etc/calamares/pkgbuild/*
-cp -r $source$dir/* $destiny
 
 # Below command will backup everything inside the project folder
 git add --all .
@@ -18,15 +36,8 @@ git commit -m "update"
 
 # Push the local files to github
 
-if grep -q main .git/config; then
-	echo "Using main"
-		git push -u origin main
-fi
-
-if grep -q master .git/config; then
-	echo "Using master"
-		git push -u origin master
-fi
+branch=$(git rev-parse --abbrev-ref HEAD)
+git push -u origin "$branch"
 
 echo
 tput setaf 6
